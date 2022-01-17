b7a8efe91c2cfe5feb4eb9cf18fbf6fb807bd4ec
Gaurav Mandana <53940031+gm088@users.noreply.github.com>
2022-01-17 10:02:37 +0100

    Add files via upload

--- spl.py
+++ spl.py
@@ -1,26 +1,37 @@
 #!/usr/bin/python3
 
+from ast import Index
+from numpy.lib.function_base import diff
 import pandas as pd
 import numpy as np
+from pandas.core.base import NoNewAttributesMixin
 import seaborn as sns
 import pyfastx as pasta
 import matplotlib.pyplot as plt
 import sys
 import os
-import tensorflow as tf
-from keras.models import load_model
-from pkg_resources import resource_filename
-from spliceai.utils import one_hot_encode
 from pybedtools import BedTool
 from maxentpy import maxent
 from maxentpy import maxent_fast
 from maxentpy.maxent import load_matrix5, load_matrix3
-
+import bedparse
+from bedparse import bedline
+import logomaker
 
 ###read and parse data from junctions.bed file
 ###store processed stuff as attributes
 ### maybe add some plotting utility functions
 ###have genome and genome index file in directory, or point to it if its elsewhere
+
+def triangle(y):
+    for i in range(2,len(y)-2):
+        y[i] = (y[i-2]+2*y[i-1]+3*y[i]+2*y[i+1]+y[i+2])/9
+    return y
+
+def csumsmooth(y, window_width):
+	cumsum_vec = np.cumsum(y)
+	ma = (cumsum_vec[window_width:] - cumsum_vec[:-window_width]) / window_width
+	return ma
 
 def allstop1(profiles):
 	#takes i x j x k array
@@ -61,8 +72,27 @@ def ploot(list,labels=None):
 	fig.patch.set_facecolor('white')
 	plt.show()
 
+def plotsome(start, labels=None, *argv):   #I think you have to pass None explicitly if you dont have labels lmao
+	
+	fig = plt.figure(figsize=(15,10))
+
+	for i in range(start,start+25):
+		plt.subplot(5,5,i+1-start)
+		#plt.xticks([])
+		plt.grid(False)
+		for j in argv:
+			plt.plot(j[i])
+		if labels:
+			#plt.xlabel("{0}".format(labels[i]))
+			plt.annotate("{0}".format(labels[i]), xy=(0.05,0.8), xycoords='axes fraction')
+		#plt.xlabel("{0}   {1:.2f}   {2:.2f}".format(regionlist[i],np.sum(indiv[i][1]),np.sum(indiv[i][2])))
+	fig.patch.set_facecolor('white')
+	plt.show()
+
 def allstop(profiles, bps, tholdcoeff=0.05):
 	#takes i x j x k array
+	if(bps<100):
+		print('binsize large; recommend big t-hold')
 
 	indiv_stop = []
 	for i in profiles:
@@ -88,10 +118,37 @@ def allstop(profiles, bps, tholdcoeff=0.05):
 						added=True
 						break
 			if not added:
-				err.append(bps/2)
+				err.append(bps/2)        #change this this is bad
 		indiv_stop.append(err)
 
 	return indiv_stop
+
+def newstop(sample,control, is_TSS=True):        ##don;t use this until I fix it this is horribly broken
+
+	#compare sample and control
+	difference = sample-control
+	ddx = np.abs(np.array(difference[1:])-np.array(difference[:-1]))
+	added=False
+
+	diff = csumsmooth(difference,5)
+	#iS_TSS is to start measuring from just downstream of TSS
+	if(is_TSS):
+		n = int(0.1*len(diff))
+		diff = diff[n:]
+
+	try:
+		index = np.where(diff <= 0)[0][0]
+	except IndexError:
+		try:
+			index = np.where(diff*np.linspace(1,0,len(diff)) <= 0)[0][0]    #squish
+		except IndexError:
+			print("defaulting")
+			index = np.where(diff[5:] == np.min(diff[5:]))[0][0]
+
+	if(is_TSS):
+		return index+n
+
+	return index
 
 def spd(indices):
 	result = []
@@ -123,10 +180,10 @@ def df2bedtool(df):
 	return BedTool(df.to_string(index=False,header=False),from_string=True)
 
 def getfastas(df):
-    obj = BedTool(df.to_string(index=False,header=False),from_string=True).sequence(fi='/home/gm089/misc/splice/genome.fa')
+    obj = BedTool(df.to_string(index=False,header=False),from_string=True).sequence(fi='/Users/IEO5559/Desktop/misc/splice/genome/genome.fa')
     return pasta.Fasta(obj.seqfn)
 
-def writefasta(seq, fname):
+def writefasta(seq, fname):    #I think this takes a list of sequences
     with open(fname,'w') as f:
         for i in range(len(seq)):
             f.write(">{0}".format(i))
@@ -135,21 +192,20 @@ def writefasta(seq, fname):
             f.write("\n")
     pass
 
-def randomly_sample_bed(fname, num):
-	bed = pd.read_table(fname,sep="\t|,",header=None)
-	for i in range(num):
-		ind = int(np.random.random_sample()*len(bed))
-		try:
-			sampledbed = sampledbed.append(bed[ind:ind+1])
-		except NameError:
-			sampledbed = pd.DataFrame(bed[ind:ind+1])
-	sampledbed = sampledbed.sort_values(by=[0,1])
-	#procbed = BedTool(sampledbed.to_string(index=False,header=False),from_string=True)
-	#del sampledbed
+def randomly_sample_bed(fname, num, juncs=True, fromfile=True):
+	if fromfile:
+		if juncs:
+			bed = pd.read_table(fname,sep="\t|,",header=None)
+		else:
+			bed = pd.read_table(fname, header=None)
+	else:
+		bed = fname
+	inds = np.random.choice(len(bed), size=num, replace=False)
+	sampledbed = bed.iloc[inds,:]
 
-	return sampledbed
+	return sampledbed.sort_values(by=[0,1])
 
-#this function is taken from spliceai.utils
+#this function is stolen from spliceai.utils
 def one_hot_encode(seq):
 
     map = np.asarray([[1, 0, 0, 0],
@@ -162,17 +218,24 @@ def one_hot_encode(seq):
 
     return map[np.fromstring(seq, np.int8) % 4]
 
-def infomat(seqlist):
+def infomat(seqlist, prob=False, show=True):
 
+	eps = np.finfo(np.float32).eps
 	enc = np.array([one_hot_encode(i) for i in seqlist])
 	baseprobs = [i/len(seqlist) for i in np.sum(enc, axis=0)]
-	u = [np.sum(-i*np.log2(i)) for i in baseprobs]
-	for i in range(len(u)):
-		if str(u[i])=='nan':
-			u[i] = 0
-	ic = 2 - np.array(u)
+	if(prob):
+		return baseprobs
+	u = [np.sum(-(i+eps)*np.log2(i+eps)) for i in baseprobs]
 
-	return [baseprobs[i]*ic[i] for i in range(len(baseprobs))]
+	ic = 2 - np.array(u)
+	info = [baseprobs[i]*ic[i] for i in range(len(baseprobs))]
+
+	if show:
+		ss = logomaker.Logo(pd.DataFrame(info,columns=['A','C','G','T']),
+		 stack_order='small_on_top', figsize=(30,5))
+		ss.ax.set_ylim([0,2])
+
+	return info
 
 def maxentcalc(donors, acceptors):
 	m5 = load_matrix5()
@@ -184,43 +247,59 @@ def maxentcalc(donors, acceptors):
 
 	return donor_scores, acceptor_scores, donor_probs, acceptor_probs
 
-def config_SAI():
+def get_introns(df, first=False):     #requires bed12 format
 
-	gpus = tf.config.list_physical_devices('GPU')
-	if gpus:
-		try:
-		# Currently, memory growth needs to be the same across GPUs
-			for gpu in gpus:
-				tf.config.experimental.set_memory_growth(gpu, True)
-		except RuntimeError as e:
-		# Memory growth must be set before GPUs have been initialized
-			print(e)
+	manyexons = df[df[9]>1]
+	print('discarded mono-exonic regions')
+	thelist = [manyexons.iloc[i,:].to_list() for i in range(len(manyexons))]
+	introns = [bedline(thelist[i]).introns() for i in range(len(thelist))]
+	if first:
+		firstintron = [i.bed12tobed6(whichExon="first")[0] for i in introns]
 
-	context = 10000
-	paths = ('models/spliceai{}.h5'.format(x) for x in range(1, 6))
-	models = [load_model(resource_filename('spliceai', x)) for x in paths]
+		return pd.DataFrame([i.listme() for i in firstintron])
+	
+	allintron = [i.bed12tobed6() for i in introns]
+	hmm = [i.listme() for j in allintron for i in j]
 
-	return context, models
+	return pd.DataFrame(hmm)
 
-def SAI_seq(sequences, context, models):			#input_sequence has to be raw sequence
-
-	dp = []
-	ap = []
-
-	for i in sequences:
-
-		input_sequence = i
-		x = one_hot_encode('N'*(context//2) + input_sequence + 'N'*(context//2))[None, :]
-		#y = np.mean([models[m].predict(x) for m in range(5)], axis=0)
-		x = tf.convert_to_tensor(x)
-		y = models[1].predict(x)
-
-		acceptor_prob = y[0, :, 1]     #mind these indices
-		donor_prob = y[0, :, 2]         #mind these indices
-		dp.append(donor_prob)
-		ap.append(acceptor_prob)
-
-	return dp, ap
+#def config_SAI():
+#
+#	gpus = tf.config.list_physical_devices('GPU')
+#	if gpus:
+#		try:
+#		# Currently, memory growth needs to be the same across GPUs
+#			for gpu in gpus:
+#				tf.config.experimental.set_memory_growth(gpu, True)
+#		except RuntimeError as e:
+#		# Memory growth must be set before GPUs have been initialized
+#			print(e)
+#
+#	context = 10000
+#	paths = ('models/spliceai{}.h5'.format(x) for x in range(1, 6))
+#	models = [load_model(resource_filename('spliceai', x)) for x in paths]
+#
+#	return context, models
+#
+#def SAI_seq(sequences, context, models):			#input_sequence has to be raw sequence
+#
+#	dp = []
+#	ap = []
+#
+#	for i in sequences:
+#
+#		input_sequence = i
+#		x = one_hot_encode('N'*(context//2) + input_sequence + 'N'*(context//2))[None, :]
+#		#y = np.mean([models[m].predict(x) for m in range(5)], axis=0)
+#		x = tf.convert_to_tensor(x)
+#		y = models[1].predict(x)
+#
+#		acceptor_prob = y[0, :, 1]     #mind these indices
+#		donor_prob = y[0, :, 2]         #mind these indices
+#		dp.append(donor_prob)
+#		ap.append(acceptor_prob)
+#
+#	return dp, ap
 
 def maxentbox(scores1, scores2):   #scores1 is donor
 	fig,axs = plt.subplots(1,2, figsize = (5,5), sharey=True)
@@ -235,9 +314,21 @@ def maxentbox(scores1, scores2):   #scores1 is donor
 	plt.xticks(range(1,2),['Second'])
 	plt.show()
 
+class experiment:
+	
+	def __init__(self):
+
+		self.plus = None
+		self.list1 = None
+		self.beddf1 = None
+		self.minus = None
+		self.list2 = None
+		self.beddf2 = None
+
+
 class junctions:
 
-	def __init__(self, fname, fromfile=True):		#fromfile false if you're passing an in-memory dataframe
+	def __init__(self, fname, fromfile=True, ohang_rem=False):		#fromfile false if you're passing an in-memory dataframe
 
 		if fromfile:
 			self.data = pd.read_table(fname, sep="\t|,", header=None)
@@ -245,7 +336,7 @@ class junctions:
 			self.data = fname
 		
 		self.rawjuncs = None
-		self.overhang_removed=False
+		self.overhang_removed=ohang_rem
 		self.win_don = None
 		self.win_acc = None
 		#self.DS, self.AS, self.DP, self.AP = None
@@ -267,27 +358,23 @@ class junctions:
 	    
 	    if(self.overhang_removed==False):
 	    	print("call remove_overhang first")
-	    	return 
+	    	return
 	    ##first remove overhang
 	    donors = self.data.copy(deep=True)              #for maxent, pass 3,9,23,3
-	    #donors[1] = donors[1]+donors[12]                #for spliceport, 80,162,162,80
-	    #donors[2] = donors[2]-donors[13]
 	    donors_plus = donors[donors[5]=="+"]
 	    donors_minus = donors[donors[5]=="-"]
-	    donors_plus[1] = donors_plus[1]-d1
-	    donors_plus[2] = donors_plus[1]+d2
-	    donors_minus[2] = donors_minus[2]+d1
-	    donors_minus[1] = donors_minus[2]-d2
+	    donors_plus.iloc[:,1] = donors_plus.iloc[:,1]-d1
+	    donors_plus.iloc[:,2] = donors_plus.iloc[:,1]+d2
+	    donors_minus.iloc[:,2] = donors_minus.iloc[:,2]+d1
+	    donors_minus.iloc[:,1] = donors_minus.iloc[:,2]-d2
 	    
 	    acceptors = self.data.copy(deep=True)
-	    #acceptors[1] = acceptors[1]+acceptors[12]
-	    #acceptors[2] = acceptors[2]-acceptors[13]
 	    acceptors_plus = acceptors[acceptors[5]=="+"]
 	    acceptors_minus = acceptors[acceptors[5]=="-"]
-	    acceptors_plus[2] = acceptors_plus[2]+a2
-	    acceptors_plus[1] = acceptors_plus[2]-a1
-	    acceptors_minus[1] = acceptors_minus[1]-a2
-	    acceptors_minus[2] = acceptors_minus[1]+a1
+	    acceptors_plus.iloc[:,2] = acceptors_plus.iloc[:,2]+a2
+	    acceptors_plus.iloc[:,1] = acceptors_plus.iloc[:,2]-a1
+	    acceptors_minus.iloc[:,1] = acceptors_minus.iloc[:,1]-a2
+	    acceptors_minus.iloc[:,2] = acceptors_minus.iloc[:,1]+a1
 
 	    self.win_don = [i.seq for i in getfastas(donors_plus)] + [i.antisense for i in getfastas(donors_minus)]
 	    self.win_acc = [i.seq for i in getfastas(acceptors_plus)] + [i.antisense for i in getfastas(acceptors_minus)]
